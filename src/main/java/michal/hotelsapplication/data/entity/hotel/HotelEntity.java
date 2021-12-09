27386e237236f36d621d327ee2a425bf63656009
Michal gutowski <michal.gutowski@tm1.edu.pl>
2021-12-09 21:14:47 +0100

    Entity for db done

---
+++ src/main/java/michal/hotelsapplication/data/entity/hotel/HotelEntity.java
@@ -0,0 +1,19 @@
+package michal.hotelsapplication.data.entity.hotel;
+
+import michal.hotelsapplication.data.entity.room.RoomEntity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "hotels")
+public class HotelEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @OneToMany(mappedBy = "room")
+    private Set<RoomEntity> rooms;
+    @Column(name = "name")
+    private String name;
+}
